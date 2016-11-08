7dd30f8fe2f2dc2b97d6fe2a33455ff1e8945290
mro9395 <mro9395@gmail.com>
2016-11-08 13:02:25 -0500

    Update webscraper-list-of-industries.py

--- webscraper-list-of-industries.py
+++ webscraper-list-of-industries.py
@@ -1,11 +1,8 @@
 from urllib.request import FancyURLopener
 from bs4 import BeautifulSoup
 
-
 class MyOpener(FancyURLopener):
     version = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11'
-
-
 myopener = MyOpener()
 
 mainURL = 'http://www.razonsocialperu.com/'
@@ -18,7 +15,6 @@ print('=' * 100)
 # Using soup
 # industries=soup.find_all('a',class_='list-group-item')
 
-
 # f=open('file-soup.txt','w')
 # test = {}
 # for element in industries:
@@ -27,14 +23,6 @@ print('=' * 100)
 # print(test[element]["link"])
 # print(element.get_text())             #works
 # f.write(element.get_text()+'\n')      #works
-
-# for item in test.keys():
-# print(item)
-# print(test[item]["link"])
-
-
-
-# print(industries)
 
 # Using split
 industries = str(soup.find_all('a', class_='list-group-item'))
@@ -50,4 +38,3 @@ f = open('file-split-urlciiu.txt', 'w')
 for x in range(0, len(industries) - 1):
     print(industries[x].split("//")[1].split('"')[0])
     f.write('http://' + industries[x].split("//")[1].split('"')[0] + '\n')
-    # url=url + industries[x].split("//")[1].split('"')[0])
